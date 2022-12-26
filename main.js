32358a4c3b0bcfd2a176a0870730554f63f2de23
andou.corp <andou.corp4@gmail.com>
2022-12-27 02:13:52 +0900

    first commit

---
+++ main.js
@@ -0,0 +1,47 @@
+//canvas セッティング
+let canvas = document.getElementById("canvas");
+let context = canvas.getContext("2d");
+
+var window_height = window.innerHeight;
+var window_width = window.innerWidth;
+
+canvas.width = window_width;
+canvas.height = window_height;
+
+canvas.style.background = "#ff8";
+
+class Circle {
+    constructor(xpos, ypos, radius, color){
+        this.xpos = xpos;
+        this.ypos = ypos;
+        this.radius = radius;
+        this.color = color;
+    }
+    draw(context){
+        context.beginPath();
+        context.lineWidth = 5;
+        context.arc(this.xpos, this.ypos, this.radius, 0, Math.PI * 2, false);
+        context.stroke();
+        context.closePath();
+    }
+}
+
+
+
+let all_circles = [];
+
+let createCircle = function(circle){
+    circle.draw(context);
+}
+
+for (var numbers = 0; numbers < 10; numbers++){
+    let random_x = Math.random() * window_width;
+    let random_y = Math.random() * window_width;
+
+    let my_circle = new Circle(random_x, random_y, 50,"black");
+    all_circles.push(my_circle);
+    createCircle(all_circles[numbers]);
+
+}
+
+   
